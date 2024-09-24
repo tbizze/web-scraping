63a8091065c60e414c5680b8d1747a59f1bbb94e
Tiago Bizze <Tiago Bizze>
2024-09-24 14:34:58 -0300

    criado view result

---
+++ resources/views/scraping/result.blade.php
@@ -0,0 +1,9 @@
+<h1 class="text-gray-700 text-lg font-bold mb-4">Converted Text</h1>
+<textarea
+    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    rows="10">{{ $text }}</textarea>
+<div class="mt-4">
+    <a href="/"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Upload
+        Another Image</a>
+</div>
