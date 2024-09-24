b7dc8b2ff8d8ce10d92cadceae44af881809d64c
Tiago Bizze <Tiago Bizze>
2024-09-24 14:16:06 -0300

    criado UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Criar superusuário
+        User::factory()->create([
+            'name'              => 'Super Admin',
+            'email'             => 'admin@test',
+            'password'          => Hash::make('123'),
+        ]);
+    }
+}
