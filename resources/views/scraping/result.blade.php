1bbe06bdc35b9aba95be0273795963a3a7daf194
Tiago Bizze <Tiago Bizze>
2024-09-25 09:15:09 -0300

    editado as views

--- resources/views/scraping/result.blade.php
+++ resources/views/scraping/result.blade.php
@@ -3,7 +3,7 @@
     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
     rows="10">{{ $text }}</textarea>
 <div class="mt-4">
-    <a href="/"
+    <a href="{{ route('image.index') }}"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Upload
         Another Image</a>
 </div>
--- resources/views/scraping/upload.blade.php
+++ resources/views/scraping/upload.blade.php
@@ -1,12 +1,28 @@
-<form action="/upload" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="image">Upload Image</label>
-        <input type="file" name="image" id="image"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Upload') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+
+                <div class="p-5">
+                    <form action="{{ route('image.upload') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-4 space-y-1">
+                            <x-label for="image">Upload Image</x-label>
+                            <input type="file" name="image" id="image"
+                                class="px-2 py-1 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <x-button>Enviar</x-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="flex items-center justify-between">
-        <button type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Convert</button>
-    </div>
-</form>
+</x-app-layout>
