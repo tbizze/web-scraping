d4896b020720b922a9fc3696e3145db99aff4e31
Tiago Bizze <Tiago Bizze>
2024-09-29 06:50:19 -0300

    update QrCodeExport: cabeçalho e retirado algumas colunas

--- app/Exports/QrCodeExport.php
+++ app/Exports/QrCodeExport.php
@@ -4,14 +4,26 @@ namespace App\Exports;
 
 use App\Models\QrCode;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class QrCodeExport implements FromCollection
+class QrCodeExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return QrCode::all();
+        return QrCode::get(['id', 'status', 'grupo', 'carne', 'pagseguro_id']);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Status',
+            'Grupo',
+            'Carnê',
+            'ID PagBank',
+        ];
     }
 }
