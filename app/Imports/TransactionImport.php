b29545b1c9ce5ef0b32700f542d4b0d35ef8a3b7
Tiago Bizze <tbizze@hotmail.com>
2024-09-30 14:24:23 -0300

    update TransactionImport

--- app/Imports/TransactionImport.php
+++ app/Imports/TransactionImport.php
@@ -4,31 +4,37 @@ namespace App\Imports;
 
 use App\Models\Transaction;
 use Carbon\Carbon;
+use DateTime;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class TransactionImport implements ToModel, WithHeadingRow
 {
-    /**
-     * @param array $row
-     *
-     * @return \Illuminate\Database\Eloquent\Model|null
-     */
+    // Método para importar os dados na tabela do BD.
     public function model(array $row)
     {
         return new Transaction([
-            'transacao_id' => $row['Transacao_ID'],
-            'tp_pgto' => $row['Tipo_Pagamento'],
-            'status' => $row['Status'],
-            'valor_bruto' => $row['Valor_Bruto'],
-            'valor_taxa' => $row['Valor_Taxa'],
-            'valor_liquido' => $row['Valor_Liquido'],
-            'dt_transacao' => Carbon::parse($row['Data_Transacao']),
-            'dt_compensacao' => Carbon::parse($row['Data_Compensacao']),
-            'ref_transacao' => $row['Ref_Transacao'],
-            'parcelas' => $row['Parcelas'],
-            'cod_venda' => $row['Codigo_Venda'],
-            'serial_leitor' => $row['Serial_Leitor'],
+            'transacao_id' => $row['transacao_id'],
+            'tp_pgto' => $row['tipo_pagamento'],
+            'status' => $row['status'],
+            'valor_bruto' => $row['valor_bruto'],
+            'valor_taxa' => $row['valor_taxa'],
+            'valor_liquido' => $row['valor_liquido'],
+            'dt_transacao' => $this->formatDate($row['data_transacao']),
+            'dt_compensacao' => $this->formatDate($row['data_compensacao']),
+            'ref_transacao' => $row['ref_transacao'],
+            'parcelas' => $row['parcelas'],
+            'cod_venda' => $row['codigo_venda'],
+            'serial_leitor' => $row['serial_leitor'],
         ]);
+    }
+
+    // Método para converter a data no formato numérico do Excel para um formato legível.
+    protected function formatDate(string $date)
+    {
+        return Date::excelToDateTimeObject($date);
     }
 }
