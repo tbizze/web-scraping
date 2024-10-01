03806db92fea26403999a861dab289e5edb8f0a0
Tiago Bizze <tbizze@hotmail.com>
2024-09-30 23:54:38 -0300

    criado métodos para tratar dados: TransactionImport

--- app/Imports/TransactionImport.php
+++ app/Imports/TransactionImport.php
@@ -2,11 +2,11 @@
 
 namespace App\Imports;
 
+use App\Models\Leitor;
+use App\Models\Status;
+use App\Models\TipoPgto;
 use App\Models\Transaction;
 use Carbon\Carbon;
-use DateTime;
-use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
@@ -18,23 +18,69 @@ class TransactionImport implements ToModel, WithHeadingRow
     {
         return new Transaction([
             'transacao_id' => $row['transacao_id'],
-            'tp_pgto' => $row['tipo_pagamento'],
-            'status' => $row['status'],
-            'valor_bruto' => $row['valor_bruto'],
-            'valor_taxa' => $row['valor_taxa'],
-            'valor_liquido' => $row['valor_liquido'],
+            'tipo_pgto_id' => $this->getTpPgto($row['tipo_pagamento']),
+            'status_id' => $this->getStatus($row['status']),
+            'valor_bruto' => $this->formatNumber($row['valor_bruto']),
+            'valor_taxa' => $this->formatNumber($row['valor_taxa']),
+            'valor_liquido' => $this->formatNumber($row['valor_liquido']),
             'dt_transacao' => $this->formatDate($row['data_transacao']),
             'dt_compensacao' => $this->formatDate($row['data_compensacao']),
             'ref_transacao' => $row['ref_transacao'],
             'parcelas' => $row['parcelas'],
             'cod_venda' => $row['codigo_venda'],
-            'serial_leitor' => $row['serial_leitor'],
+            'leitor_id' => $this->getLeitor($row['serial_leitor']),
         ]);
+    }
+
+    // Método para converter números em formato legível.
+    protected function formatNumber($number)
+    {
+        if ($number) {
+            return (float) str_replace(['.', ','], ['', '.'], $number);
+        }
     }
 
     // Método para converter a data no formato numérico do Excel para um formato legível.
     protected function formatDate(string $date)
     {
+        if (Carbon::hasFormat($date, 'd/m/Y H:i')) {
+            return Carbon::createFromFormat('d/m/Y H:i', $date)->format('Y-m-d H:i');
+        }
         return Date::excelToDateTimeObject($date);
+    }
+
+    // Método para inserir tipo de pagamento no BD, resgatando ID.
+    private function getTpPgto(string $tp_pgto)
+    {
+        // Se não existir, cria um novo.
+        $data = TipoPgto::firstOrCreate(
+            ['description' =>  $tp_pgto],
+        );
+        return $data->id;
+    }
+
+    // Método para inserir status no BD, resgatando o ID.
+    private function getStatus(string $status)
+    {
+        // Se não existir, cria um novo.
+        $data = Status::firstOrCreate(
+            ['description' =>  $status],
+        );
+        return $data->id;
+    }
+
+    // Método para inserir Leitor no BD, resgatando o ID.
+    private function getLeitor(string|null $leitor)
+    {
+        // Se variável for null, retorna null.
+        if ($leitor == null) {
+            return null;
+        }
+
+        // Se não existir, cria um novo.
+        $data = Leitor::firstOrCreate(
+            ['description' =>  $leitor],
+        );
+        return $data->id;
     }
 }
