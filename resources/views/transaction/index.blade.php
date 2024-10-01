d790cc3bb8297cc58856e2fb628b9fefe2ab431d
Tiago Bizze <Tiago Bizze>
2024-10-01 12:40:28 -0300

    melhorias view transaction

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -13,58 +13,62 @@
                 <div class="mx-5 my-5 text-gray-900 dark:text-gray-100 space-y-3">
 
                     {{-- Chama compomente para exibir flesh message --}}
-                    <x-flash-message />
+                    <x-biz.flash-message />
 
-                    <div class="flex gap-2">
-                        <a href="{{ route('transactions.import') }}"
-                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                            Importar Excel
-                        </a>
-                        <a href="{{ route('transactions.import-all') }}"
-                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                            Varrer Diretório
-                        </a>
-
-                        <form action="{{ route('transactions.index') }}" method="GET">
-                            <div class="flex flex-wrap -mx-3 gap-2">
-                                @csrf
-                                <div class="">
-                                    <select
-                                        class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="tp_pgto_id" name="tp_pgto_id">
-                                        <option selected value="">Tipo Pgto:</option>
-                                        @foreach ($tpPgtos as $item)
-                                            <option value="{{ $item->id }}"
-                                                {{ $item->id == $tp_pgto_id ? 'selected' : '' }}>
-                                                {{ $item->description }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="">
-                                    <select
-                                        class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="status_id" name="status_id">
-                                        <option selected value="">Status:</option>
-                                        @foreach ($statuses as $item)
-                                            <option value="{{ $item->id }}"
-                                                {{ $item->id == $status_id ? 'selected' : '' }}>
-                                                {{ substr($item->description, 0, 20) }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <x-button>Filtrar</x-button>
-
+                    <div class="flex justify-between">
+                        {{-- Lado Esquerdo: botões --}}
+                        <div class="">
+                            <div class="flex gap-2">
+                                <x-biz.link href="{{ route('transactions.import') }}" class="py-3">
+                                    Importar Excel
+                                </x-biz.link>
+                                <x-biz.link href="{{ route('transactions.import-all') }}" class="py-3">
+                                    Varrer Diretório
+                                </x-biz.link>
                             </div>
-                        </form>
-
+                        </div>
+                        {{-- Lado Direito: filtros --}}
+                        <div class="">
+                            <form action="{{ route('transactions.index') }}" method="GET" class="ml-4">
+                                <div class="flex flex-wrap gap-2">
+                                    @csrf
+                                    <div class="">
+                                        <select
+                                            class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded-md leading-4 text-sm focus:outline-none focus:bg-white focus:border-gray-500"
+                                            id="tp_pgto_id" name="tp_pgto_id">
+                                            <option selected value="">Tipo Pgto:</option>
+                                            @foreach ($tpPgtos as $item)
+                                                <option value="{{ $item->id }}"
+                                                    {{ $item->id == $tp_pgto_id ? 'selected' : '' }}>
+                                                    {{ $item->description }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="">
+                                        <select
+                                            class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded-md leading-4 text-sm focus:outline-none focus:bg-white focus:border-gray-500"
+                                            id="status_id" name="status_id">
+                                            <option selected value="">Status:</option>
+                                            @foreach ($statuses as $item)
+                                                <option value="{{ $item->id }}"
+                                                    {{ $item->id == $status_id ? 'selected' : '' }}>
+                                                    {{ substr($item->description, 0, 20) }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <x-button>Filtrar</x-button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+
 
                     <table class="min-w-full w-full table-auto">
                         <thead>
                             <tr class="bg-slate-600 ">
-                                <th class="">Tipo</th>
+                                <th class="py-3">Tipo</th>
                                 <th class="">Status</th>
                                 <th class="">Bruto</th>
                                 <th class="">Taxa</th>
@@ -74,7 +78,7 @@
                                 <th class="">Parcela</th>
                                 <th class="">Ref.</th>
                                 <th class="">Cód. Venda</th>
-                                <th class="">Leitor</th>
+                                <th class="pr-2">Leitor</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -95,7 +99,7 @@
                                     <td class="px-4 py-2 text-right">
                                         {{ $item->valor_liquido }}
                                     </td>
-                                    <td class="px-4 py-2 ">
+                                    <td class="px-4 py-2 text-center">
                                         {{ $item->dt_transacao }}
                                     </td>
                                     <td class="px-4 py-2 text-center">
