191e30a8e8417661128575f14a241312d58dd528
Tiago Bizze <tbizze@hotmail.com>
2024-09-30 23:51:06 -0300

    preparado models: TipoPgto, Leitor, Status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Status extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['description', 'notes'];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
