2f4eb4969873c92cba097e13718db903ff7e9b8b
Tiago Bizze <Tiago Bizze>
2024-10-02 10:23:56 -0300

    corrigido view

--- resources/views/transaction/import.blade.php
+++ resources/views/transaction/import.blade.php
@@ -13,12 +13,13 @@
                 <div class="mx-5 my-5 text-gray-900 dark:text-gray-100 space-y-3">
 
                     {{-- Chama compomente para exibir flesh message --}}
-                    <x-flash-message />
+                    <x-biz.flash-message />
+                    <h3 class="text-xl font-medium text-gray-800 dark:text-gray-200">Importar transações</h3>
 
                     <form action="{{ route('transactions.process.import') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-4 space-y-1">
-                            <x-label for="image">Escolha o arquivo Excel</x-label>
+                            <x-label for="image">Escolha arquivo Excel com relatório de transações PagBank.</x-label>
                             <input type="file" name="file"
                                 class="px-2 py-1 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                             @error('file')
