ed0d4fb29202a7ce7719098301a757c6c734f88d
Tiago Bizze <Tiago Bizze>
2024-10-03 10:23:58 -0300

    criado relacionamento -> transactions

--- app/Models/QrCode.php
+++ app/Models/QrCode.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class QrCode extends Model
 {
@@ -13,8 +12,12 @@ class QrCode extends Model
 
     protected $fillable = ['path', 'name_file', 'status', 'content', 'grupo', 'carne', 'pagseguro_id', 'transaction_id'];
 
-    public function transaction(): BelongsTo
+    // public function transaction(): BelongsTo
+    // {
+    //     return $this->belongsTo(Transaction::class)->withDefault('N/D');
+    // }
+    public function transactions(): HasMany
     {
-        return $this->belongsTo(Transaction::class)->withDefault('N/D');
+        return $this->hasMany(Transaction::class);
     }
 }
